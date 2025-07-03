d16130f5ca9762573063f57e2db48fc1e5c806c1
Vladimir Marinov <vlad24@abv.bg>
2025-07-03 22:16:42 +0300

    4

--- ShipmentSolution.Services.Core/CustomerService .cs
+++ ShipmentSolution.Services.Core/CustomerService .cs
@@ -7,7 +7,6 @@ using ShipmentSolution.Web.ViewModels.CustomerViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace ShipmentSolution.Services.Core
