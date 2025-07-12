c1bd5e5b4d26dadc7a6d1a6e1a2cb0d81631614d
Vladimir Marinov <vlad24@abv.bg>
2025-07-12 17:16:09 +0300

    17

---
+++ ShipmentSolution.Tests/Controllers/MailCarrierControllerTests.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using ShipmentSolution.Services.Core.Interfaces;
+using ShipmentSolution.Web.Controllers;
+using ShipmentSolution.Web.ViewModels.Common;
+using ShipmentSolution.Web.ViewModels.MailCarrierViewModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShipmentSolution.Tests.Controllers
+{
+    public class MailCarrierControllerTests
+    {
+        private Mock<IMailCarrierService> _mailCarrierServiceMock;
+#pragma warning disable NUnit1032
+        private Mock<ILogger<MailCarrierController>> _loggerMock;
+//#pragma warning restore NUnit1032
+        private MailCarrierController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mailCarrierServiceMock = new Mock<IMailCarrierService>();
+            _loggerMock = new Mock<ILogger<MailCarrierController>>();
+            _controller = new MailCarrierController(_mailCarrierServiceMock.Object, _loggerMock.Object);
+        }
+
+        [Test]
+        public async Task Index_ReturnsViewResult_WithMailCarriers()
+        {
+            var mailCarriers = new List<MailCarrierViewModel>
+            {
+                new MailCarrierViewModel { MailCarrierId = 1, FullName = "John", Status = "Active" },
+                new MailCarrierViewModel { MailCarrierId = 2, FullName = "Jane", Status = "Inactive" }
+            };
+
+            _mailCarrierServiceMock.Setup(s => s.GetPaginatedAsync(1, 5, null, null))
+                .ReturnsAsync(new PaginatedList<MailCarrierViewModel> { Items = mailCarriers });
+
+            _mailCarrierServiceMock.Setup(s => s.GetAllAsync())
+                .ReturnsAsync(mailCarriers);
+
+            var result = await _controller.Index(null, null);
+
+            Assert.That(result, Is.InstanceOf<ViewResult>());
+            var viewResult = result as ViewResult;
+            Assert.That(viewResult!.Model, Is.InstanceOf<PaginatedList<MailCarrierViewModel>>());
+        }
+
+        [Test]
+        public void Create_Get_ReturnsView()
+        {
+            var result = _controller.Create();
+            Assert.That(result, Is.InstanceOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task Create_Post_InvalidModel_ReturnsView()
+        {
+            _controller.ModelState.AddModelError("FullName", "Required");
+
+            var result = await _controller.Create(new MailCarrierCreateViewModel());
+
+            Assert.That(result, Is.InstanceOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task Create_Post_ValidModel_RedirectsToIndex()
+        {
+            var model = new MailCarrierCreateViewModel { FullName = "John Doe" };
+
+            _mailCarrierServiceMock.Setup(s => s.CreateAsync(model)).Returns(Task.CompletedTask);
+
+            var result = await _controller.Create(model);
+
+            Assert.That(result, Is.InstanceOf<RedirectToActionResult>());
+            Assert.That(((RedirectToActionResult)result).ActionName, Is.EqualTo("Index"));
+        }
+
+        [Test]
+        public async Task Edit_Get_ReturnsViewWithModel()
+        {
+            var model = new MailCarrierEditViewModel { MailCarrierId = 1, FullName = "Test" };
+            _mailCarrierServiceMock.Setup(s => s.GetForEditAsync(1)).ReturnsAsync(model);
+
+            var result = await _controller.Edit(1);
+
+            Assert.That(result, Is.InstanceOf<ViewResult>());
+            Assert.That(((ViewResult)result).Model, Is.EqualTo(model));
+        }
+
+        [Test]
+        public async Task Edit_Post_InvalidModel_ReturnsView()
+        {
+            _controller.ModelState.AddModelError("FullName", "Required");
+
+            var result = await _controller.Edit(new MailCarrierEditViewModel());
+
+            Assert.That(result, Is.InstanceOf<ViewResult>());
+        }
+
+        [Test]
+        public async Task Edit_Post_ValidModel_RedirectsToIndex()
+        {
+            var model = new MailCarrierEditViewModel { MailCarrierId = 1, FullName = "Updated" };
+
+            _mailCarrierServiceMock.Setup(s => s.EditAsync(model)).Returns(Task.CompletedTask);
+
+            var result = await _controller.Edit(model);
+
+            Assert.That(result, Is.InstanceOf<RedirectToActionResult>());
+            Assert.That(((RedirectToActionResult)result).ActionName, Is.EqualTo("Index"));
+        }
+
+        
+
+        [Test]
+        public async Task DeleteConfirmed_DeletesAndRedirects()
+        {
+            _mailCarrierServiceMock.Setup(s => s.SoftDeleteAsync(1)).Returns(Task.CompletedTask);
+
+            var result = await _controller.DeleteConfirmed(1);
+
+            Assert.That(result, Is.InstanceOf<RedirectToActionResult>());
+            Assert.That(((RedirectToActionResult)result).ActionName, Is.EqualTo("Index"));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _mailCarrierServiceMock = null!;
+            _loggerMock = null!;
+            _controller = null!;
+        }
+    }
+}
---
+++ ShipmentSolution.Tests/Controllers/RouteControllerTests.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using ShipmentSolution.Services.Core.Interfaces;
+using ShipmentSolution.Web.Controllers;
+using ShipmentSolution.Web.ViewModels.RouteViewModels;
+using ShipmentSolution.Web.ViewModels.Common;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ShipmentSolution.Tests.Controllers
+{
+    public class RouteControllerTests
+    {
+        private Mock<IRouteService> _routeServiceMock;
+        private Mock<ILogger<RouteController>> _loggerMock;
+#pragma warning disable NUnit1032
+        private RouteController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _routeServiceMock = new Mock<IRouteService>();
+            _loggerMock = new Mock<ILogger<RouteController>>();
+            _controller = new RouteController(_routeServiceMock.Object, _loggerMock.Object);
+        }
+
+        [Test]
+        public async Task Index_ReturnsViewResult_WithModel()
+        {
+            var pagedRoutes = new PaginatedList<RouteViewModel>
+            {
+                Items = new List<RouteViewModel>
+                {
+                    new RouteViewModel { RouteId = 1, StartLocation = "A", EndLocation = "B", Priority = "High" }
+                }
+            };
+
+            _routeServiceMock.Setup(s => s.GetPaginatedAsync(1, 5, null, null))
+                .ReturnsAsync(pagedRoutes);
+
+            var result = await _controller.Index(null, null);
+
+            Assert.That(result, Is.InstanceOf<ViewResult>());
+            var viewResult = result as ViewResult;
+            Assert.That(viewResult!.Model, Is.InstanceOf<PaginatedList<RouteViewModel>>());
+        }
+
+        
+
+        [Test]
+        public async Task Delete_Get_ReturnsViewWithModel()
+        {
+            var deleteModel = new RouteDeleteViewModel { RouteId = 1, StartLocation = "A", EndLocation = "B" };
+            _routeServiceMock.Setup(s => s.GetDeleteViewModelAsync(1)).ReturnsAsync(deleteModel);
+
+            var result = await _controller.Delete(1);
+
+            Assert.That(result, Is.InstanceOf<ViewResult>());
+            Assert.That(((ViewResult)result).Model, Is.EqualTo(deleteModel));
+        }
+
+        [Test]
+        public async Task DeleteConfirmed_DeletesAndRedirects()
+        {
+            _routeServiceMock.Setup(s => s.SoftDeleteAsync(1)).Returns(Task.CompletedTask);
+
+            var result = await _controller.DeleteConfirmed(1);
+
+            Assert.That(result, Is.InstanceOf<RedirectToActionResult>());
+            Assert.That(((RedirectToActionResult)result).ActionName, Is.EqualTo("Index"));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _routeServiceMock = null!;
+            _loggerMock = null!;
+            _controller = null!;
+        }
+    }
+}
